f0e363ef84f5b9a0717051de0e4c725dc0576a17
Beverley Sun <beverleysun0205@gmail.com>
2020-08-21 20:29:11 +1200

    Loading in question data

--- src/jeopardy/Controller.java
+++ src/jeopardy/Controller.java
@@ -1,6 +1,8 @@
 package jeopardy;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Controller {
@@ -8,16 +10,30 @@ public class Controller {
     private final File _answeredFolder = new File("./.save/answered");
     private final File _winningsFile = new File("./.save/winnings");
     private final File _categoriesFolder = new File("./categories");
+    private final File[] _categoryFiles = _categoriesFolder.listFiles();
 
     public void loadQuestions() throws IOException {
+
+        // Create file structure if it doesn't exist
         if(!_saveFolder.exists()) {
-            // Create file structure
             _answeredFolder.mkdirs();
             _winningsFile.createNewFile();
 
             // Create folders for each category in the save folder
             for (String name : _categoriesFolder.list()) {
                 new File("./.save/answered/" + name).mkdir();
+            }
+        }
+
+        for(File categoryFile : _categoryFiles) {
+            BufferedReader reader = new BufferedReader(new FileReader(categoryFile));
+            String questionLine;
+
+            while((questionLine = reader.readLine()) != null) {
+                String[] questionData = questionLine.split(",");
+                int value = Integer.parseInt(questionData[0].trim());
+                String question = questionData[1].trim();
+                String answer = questionData[2].trim();
             }
         }
     }
