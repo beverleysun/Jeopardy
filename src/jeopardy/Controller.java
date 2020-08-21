4a89d1fb455e1672269597171916b4286ff28ead
Beverley Sun <beverleysun0205@gmail.com>
2020-08-21 20:52:25 +1200

    Complete loading of data

--- src/jeopardy/Controller.java
+++ src/jeopardy/Controller.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
     private final File _saveFolder = new File("./.save");
@@ -11,6 +13,8 @@ public class Controller {
     private final File _winningsFile = new File("./.save/winnings");
     private final File _categoriesFolder = new File("./categories");
     private final File[] _categoryFiles = _categoriesFolder.listFiles();
+
+    private final List<Category> _questionData = new ArrayList<>();
 
     public void loadQuestions() throws IOException {
 
@@ -26,15 +30,30 @@ public class Controller {
         }
 
         for(File categoryFile : _categoryFiles) {
+            // Read every line of category file
             BufferedReader reader = new BufferedReader(new FileReader(categoryFile));
+            Category category = new Category(categoryFile.getName());
             String questionLine;
 
             while((questionLine = reader.readLine()) != null) {
                 String[] questionData = questionLine.split(",");
+
+                // Extract information from the question line
                 int value = Integer.parseInt(questionData[0].trim());
                 String question = questionData[1].trim();
                 String answer = questionData[2].trim();
+
+                // Check if question has been answered
+                boolean answered = isAnswered(categoryFile.getName(), value);
+
+                category.addQuestion(new Question(question, answer, value, answered));
             }
+
+            _questionData.add(category);
         }
+    }
+
+    private boolean isAnswered(String category, int value) {
+        return new File("./.save/answered/" + category + "/" + value).exists();
     }
 }
