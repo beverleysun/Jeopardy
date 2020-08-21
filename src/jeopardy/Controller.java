9665de69f97ea872fbcf517cff568b15e45c69b3
Beverley Sun <beverleysun0205@gmail.com>
2020-08-21 20:56:33 +1200

    Create winnings file if it doesn't exist

--- src/jeopardy/Controller.java
+++ src/jeopardy/Controller.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Controller {
     private final File _saveFolder = new File("./.save");
     private final File _answeredFolder = new File("./.save/answered");
-    private final File _winningsFile = new File("./.save/winnings");
+    private final File _winningsFolder = new File("./.save/winnings");
     private final File _categoriesFolder = new File("./categories");
     private final File[] _categoryFiles = _categoriesFolder.listFiles();
 
@@ -21,7 +21,8 @@ public class Controller {
         // Create file structure if it doesn't exist
         if(!_saveFolder.exists()) {
             _answeredFolder.mkdirs();
-            _winningsFile.createNewFile();
+            _winningsFolder.mkdir();
+            new File("./.save/winnings/0").createNewFile();
 
             // Create folders for each category in the save folder
             for (String name : _categoriesFolder.list()) {
