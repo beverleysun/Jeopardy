27d34401680547eee59064c368c7a7832af3a54c
Beverley Sun <beverleysun0205@gmail.com>
2020-08-22 22:38:19 +1200

    Move to SceneGenerator

--- src/jeopardy/Jeopardy.java
+++ src/jeopardy/Jeopardy.java
@@ -1,12 +1,8 @@
 package jeopardy;
 
 import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Jeopardy extends Application {
@@ -17,43 +13,9 @@ public class Jeopardy extends Application {
 
     @Override
     public void start(Stage stage) {
+        Scene scene = new Scene(new Pane(), 900, 600);
+        Scene startScene = SceneGenerator.getStartScene(stage, scene);
         stage.setTitle("Jeopardy");
-        showStartScene(stage);
-    }
-
-    public void showStartScene(Stage stage) {
-        GridPane root = new GridPane();
-        Scene scene = new Scene(root, 900, 600);
-
-        // Components
-        Label jeopardyLabel = new Label("JEOPARDY");
-        Button startButton = new Button("Start");
-        Button resetButton = new Button("Reset");
-
-        // Alignment of content
-        root.setAlignment(Pos.CENTER);
-        root.setVgap(20);
-        GridPane.setHalignment(startButton, HPos.CENTER);
-        GridPane.setHalignment(resetButton, HPos.CENTER);
-        GridPane.setHalignment(jeopardyLabel, HPos.CENTER);
-
-        // Buttons
-        startButton.setPrefWidth(100);
-        startButton.setOnAction(new StartButtonHandler(stage, scene));
-
-        resetButton.setPrefWidth(100);
-
-        // Set styles
-        scene.getStylesheets().add("style.css");
-        jeopardyLabel.setId("start-jeopardy");
-        startButton.setId("start-button");
-        resetButton.setId("start-button");
-        root.setId("background");
-
-        // Add components to the scene
-        root.addColumn(0, jeopardyLabel, startButton, resetButton);
-
-        stage.setScene(scene);
-        stage.show();
+        Controller.showScene(stage, startScene);
     }
 }
