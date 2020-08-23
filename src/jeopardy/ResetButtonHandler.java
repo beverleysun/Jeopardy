41811ba612ac5092a5b458bdd37f5ba8f461b81d
Beverley Sun <beverleysun0205@gmail.com>
2020-08-23 15:19:20 +1200

    Use superclass instances

--- src/jeopardy/ResetButtonHandler.java
+++ src/jeopardy/ResetButtonHandler.java
@@ -16,15 +16,15 @@ public class ResetButtonHandler extends ButtonHandler implements EventHandler {
     public void handle(Event event) {
         Button button = (Button) event.getSource();
         if (button.getId().equals("reset-button")) {
-            Scene resetScene = SceneGenerator.getInstance().getResetScene(_stage, _scene);
-            Controller.getInstance().showScene(_stage, resetScene);
+            Scene resetScene = _sceneGenerator.getResetScene(_stage, _scene);
+            _controller.showScene(_stage, resetScene);
         } else if (button.getId().equals("yes-button")) {
             Controller.getInstance().reset();
-            Scene startScene = SceneGenerator.getInstance().getStartScene(_stage, _scene);
-            Controller.getInstance().showScene(_stage, startScene);
+            Scene startScene = _sceneGenerator.getStartScene(_stage, _scene);
+            _controller.showScene(_stage, startScene);
         } else {
-            Scene startScene = SceneGenerator.getInstance().getStartScene(_stage, _scene);
-            Controller.getInstance().showScene(_stage, startScene);
+            Scene startScene = _sceneGenerator.getStartScene(_stage, _scene);
+            _controller.showScene(_stage, startScene);
         }
     }
 }
