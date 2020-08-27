1300f6885c7b5e014e3902c87e2cd884b38562d4
Beverley Sun <beverleysun0205@gmail.com>
2020-08-27 22:25:28 +1200

    Handle back to question board as well

--- src/jeopardy/BackButtonHandler.java
+++ src/jeopardy/BackButtonHandler.java
@@ -2,6 +2,7 @@ package jeopardy;
 
 import javafx.event.Event;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class BackButtonHandler extends ButtonHandler {
@@ -12,7 +13,16 @@ public class BackButtonHandler extends ButtonHandler {
 
     @Override
     public void handle(Event event) {
-        Scene startScene = _sceneGenerator.getStartScene(_stage, _scene);
-        _controller.showScene(_stage, startScene);
+        Button button = (Button) event.getSource();
+        Scene scene;
+
+        if (button.getId().equals("to-start")) {
+            scene = _sceneGenerator.getStartScene(_stage, _scene);
+        } else {
+            scene = _sceneGenerator.getQuestionBoardScene(_stage, _scene);
+        }
+
+        _controller.showScene(_stage, scene);
+
     }
 }
