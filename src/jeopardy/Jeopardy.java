10e402eff7b1411314884dd277e9423c68be295b
Beverley Sun <beverleysun0205@gmail.com>
2020-08-28 20:53:34 +1200

    Commenting

--- src/jeopardy/Jeopardy.java
+++ src/jeopardy/Jeopardy.java
@@ -15,15 +15,19 @@ public class Jeopardy extends Application {
 
     @Override
     public void start(Stage stage) {
+        // Load assets
         Font.loadFont(getClass().getResourceAsStream("/assets/slkscr.ttf"), 16);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/assets/logo.png")));
+
+        // Set up stage area
         Scene scene = new Scene(new Pane(), 900, 600);
         SceneGenerator sceneGenerator = SceneGenerator.getInstance();
         Scene startScene = sceneGenerator.getStartScene(stage, scene);
         stage.setTitle("Jeopardy!");
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("../assets/logo.png")));
-
         stage.setMinHeight(600);
         stage.setMinWidth(Controller.getInstance().getNumCats()*200);
+
+        // Show
         Controller.getInstance().showScene(stage, startScene);
     }
 }
