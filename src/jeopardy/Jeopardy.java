f620b481f57a33c5bc7bdd8b64c59a0a75cfae62
Beverley Sun <beverleysun0205@gmail.com>
2020-08-28 20:32:01 +1200

    Load new font and icon

--- src/jeopardy/Jeopardy.java
+++ src/jeopardy/Jeopardy.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Jeopardy extends Application {
@@ -14,11 +15,13 @@ public class Jeopardy extends Application {
 
     @Override
     public void start(Stage stage) {
+        Font.loadFont(getClass().getResourceAsStream("/assets/slkscr.ttf"), 16);
         Scene scene = new Scene(new Pane(), 900, 600);
         SceneGenerator sceneGenerator = SceneGenerator.getInstance();
         Scene startScene = sceneGenerator.getStartScene(stage, scene);
         stage.setTitle("Jeopardy!");
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("../logo.png")));
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("../assets/logo.png")));
+
         stage.setMinHeight(600);
         stage.setMinWidth(Controller.getInstance().getNumCats()*200);
         Controller.getInstance().showScene(stage, startScene);
